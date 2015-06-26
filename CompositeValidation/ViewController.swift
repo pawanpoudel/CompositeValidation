b85561e5e0b0386a963b91d37b85cc44abac573c
Pawan Poudel <pawan.poudel@gmail.com>
2015-06-25 20:04:38 -0400

    Create a new Xcode project with single view app template

---
+++ CompositeValidation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CompositeValidation
+//
+//  Created by Pawan Poudel on 6/25/15.
+//  Copyright (c) 2015 Pawan Poudel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
